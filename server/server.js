1e21b1607186d19492a33cb1dbd4383b53ebcfb7
Talha-Rasool <talharasool450@gmail.com>
2025-04-10 08:17:31 +0200

    fix bug regarding port issue with backend

--- server/server.js
+++ server/server.js
@@ -23,8 +23,8 @@ app.use(express.json());
 connectDB()
   .then(() => {
     // Start server only after DB connection
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+    app.listen(5001, () => {
+      console.log("Server running on port 5001");
     });
   })
   .catch((err) => {
@@ -55,5 +55,5 @@ connectDB()
 app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
